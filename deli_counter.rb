41b730262399293c7e7753d591ee945e0cb51e24
Muhammad Afif Alaptagin <afiffifa89@gmail.com>
2019-12-22 09:14:52 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,12 +5,13 @@ def line(array)
   if array.length == 0 
     puts "The line is currently empty"
   else 
-    massage = "The line is currently:"
+    message = "The line is currently:"
     array.each_with_index do |value, index|
       message += " #{index.to_i+1}. #{value}"
     end
-    puts "#{massage}"
-
+    puts "#{message}"
+    
+    
 def take_a_number(array, name)
   array.push(name)
   position = array.index(name)
